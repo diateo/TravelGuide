02384b0de20e94463449523785c1bf27c0298d65
Diana Grzelak <tdianateodora@gmail.com>
2023-04-03 20:48:29 +0200

    setup the layout boilerplate and add Bootstrap

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Attraction = require('./models/attraction');
 
@@ -17,6 +18,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/travel-guide')
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
