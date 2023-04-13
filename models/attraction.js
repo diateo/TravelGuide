056a4201f66c94defa6261443281fc01710c2b67
Diana Grzelak <tdianateodora@gmail.com>
2023-04-13 23:55:40 +0200

    create the 'one-to-many' relationship between the models

--- models/attraction.js
+++ models/attraction.js
@@ -7,7 +7,12 @@ const AttractionSchema = new Schema({
     image: String,
     location: String,
     description: String,
-    
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref:'Review'
+        }
+    ]
 });
 
 module.exports = mongoose.model('Attraction', AttractionSchema);
