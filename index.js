dd456c8b96df8aea4bff876aaebc634d1395e096
Diana Grzelak <tdianateodora@gmail.com>
2023-04-15 20:08:29 +0200

    add form and route to delete a particulat review

--- index.js
+++ index.js
@@ -105,6 +105,13 @@ app.post('/attractions/:id/reviews', reviewValidation, catchAsync(async (req, re
 
 }))
 
+app.delete('/attractions/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Attraction.findByIdAndUpdate(id,{$pull: {reviews:reviewId}})
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/attractions/${id}`);
+}))
+
 app.all('*', (req, res, next) => {
     next(new ExpressError(404, 'Page not found'));
 })
