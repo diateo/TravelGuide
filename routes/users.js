6bfe07371122583b4bda90e9e9413fe807935dae
Diana Grzelak <tdianateodora@gmail.com>
2023-04-25 01:18:45 +0200

    create a logout route

--- routes/users.js
+++ routes/users.js
@@ -8,13 +8,16 @@ router.get('/register', (req, res) => {
     res.render('users/register');
 })
 
-router.post('/register', catchAsync(async (req, res) => {
+router.post('/register', catchAsync(async (req, res,next) => {
     try {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
-        req.flash('success','Congrats! Welcome to Romania travel guide!')
-        res.redirect('/attractions');
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'Congrats! Welcome to Romania travel guide!');
+            res.redirect('/attractions');
+        });
     } catch (e){
         req.flash('error', e.message);
         res.redirect('/register');
@@ -31,6 +34,12 @@ router.post('/login',passport.authenticate('local',{failureFlash:true,failureRed
     res.redirect('/attractions');
 })
 
-
+router.post('/logout', (req, res,next) => {
+    req.logout(err=>{
+        if (err) return next(err);
+        req.flash('success', 'See you next time :)');
+        res.redirect('/attractions');
+    })
+})
 
 module.exports = router;
