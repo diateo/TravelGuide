b50ae5053b6c1a044bea31b66d24b07ea650b839
Diana Grzelak <tdianateodora@gmail.com>
2023-04-25 23:43:13 +0200

    add an author to the review model

--- models/review.js
+++ models/review.js
@@ -3,7 +3,11 @@ const Schema = mongoose.Schema;
 
 const ReviewSchema = new Schema({
     text: String,
-    rating: Number
+    rating: Number,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref:'User'
+    }
 });
 
 module.exports=mongoose.model('Review', ReviewSchema);
