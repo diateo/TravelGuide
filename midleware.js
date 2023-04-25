b1edcc13cd17d649349e34e2ab0e447cad5716b5
Diana Grzelak <tdianateodora@gmail.com>
2023-04-25 23:22:27 +0200

    create midleware to check if user is the owner of the attraction

--- midleware.js
+++ midleware.js
@@ -1,3 +1,7 @@
+const { attractionSchema,reviewSchema } = require('./validationSchemas');
+const ExpressError = require('./utilities/ExpressError');
+const Attraction = require('./models/attraction');
+
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.session.returnTo = req.originalUrl;
@@ -10,6 +14,40 @@ module.exports.isLoggedIn = (req, res, next) => {
 module.exports.storeReturnTo = (req, res, next) => {
     if (req.session.returnTo) {
         res.locals.returnTo = req.session.returnTo;
+    }
+    next();
+}
+
+//create midleware for the JOI validation schema 
+module.exports.attractionValidation = (req, res, next) => {
+    const {error} = attractionSchema.validate(req.body);
+    if (error) {
+        const finalMessage = error.details.map(msg => msg.message).join(',');
+        throw new ExpressError(400, finalMessage);
+    }
+    else {
+        next();
+    }
+}
+
+module.exports.reviewValidation = (req, res, next) => {
+    const {error} = reviewSchema.validate(req.body);
+    if (error) {
+        const finalMessage = error.details.map(msg => msg.message).join(',');
+        throw new ExpressError(400, finalMessage);
+    }
+    else {
+        next();
+    }
+}
+
+
+module.exports.isOwner = async (req, res, next) => {
+    const { id } = req.params;
+    const attraction = await Attraction.findById(id);
+    if (!attraction.owner.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/attractions/${id}`);
     }
     next();
 }
