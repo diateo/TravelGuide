9dc84af13ac35f47ad489b9a4fc9461fdffaac83
Diana Grzelak <tdianateodora@gmail.com>
2023-05-08 21:18:00 +0200

    display thumbnails when editing the attraction with the help of a virtual propery and the Cloudinary image transformation API

--- models/attraction.js
+++ models/attraction.js
@@ -2,15 +2,22 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Review = require('./review');
 
+
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
+
 const AttractionSchema = new Schema({
     name: String,
     fee: Number,
-    images: [
-        {
-            url: String,
-            filename:String
-        }
-    ],
+    images: [ImageSchema],
     location: String,
     description: String,
     owner: {
