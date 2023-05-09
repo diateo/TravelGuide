96ee4c89a15d19ed73d99062be8939842cc943c7
Diana Grzelak <tdianateodora@gmail.com>
2023-05-09 06:59:45 +0200

    sanitize HTML with JOI

--- validationSchemas.js
+++ validationSchemas.js
@@ -1,12 +1,35 @@
-const joi = require('joi');
+const baseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const joi = baseJoi.extend(extension);
 
 module.exports.attractionSchema = joi.object({
     attraction: joi.object({
-        name: joi.string().required(),
+        name: joi.string().required().escapeHTML(),
         fee: joi.number().required().min(0),
-        location: joi.string().required(),
+        location: joi.string().required().escapeHTML(),
        // image: joi.string().required(),
-        description:joi.string().required()
+        description:joi.string().required().escapeHTML()
     }).required(),
     deleteImages:joi.array()
 });
@@ -14,7 +37,7 @@ module.exports.attractionSchema = joi.object({
 
 module.exports.reviewSchema = joi.object({
     review: joi.object({
-        text: joi.string().required(),
+        text: joi.string().required().escapeHTML(),
         rating: joi.number().required().min(1).max(5)
     }).required()
 });
