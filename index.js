e2a4a4470b452e9f88de7d9ac90c9a9e209c5f0d
Diana Grzelak <tdianateodora@gmail.com>
2023-05-09 07:04:16 +0200

    rename the session

--- index.js
+++ index.js
@@ -45,12 +45,13 @@ app.use(mongoSanitize());
 
 
 const sessionConfiguration = {
-
+    name: 'blablabla',
     secret: 'temporarysecret',
     resave: false,
     saveUninitialized: true,
     cookie: {
-        httpOnly:true,
+        httpOnly: true,
+        //secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
